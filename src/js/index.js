e104dcb3db23a49389deda3ec47a9206c9689314
jlamazares <jlamazares01@gmail.com>
2019-09-21 00:45:09 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,32 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excusegenerator").innerHTML = generateExcuse();
 };
+
+function generateExcuse() {
+  let who = ["the dog", "my grandma", "the mafia", "a sad clown"];
+  let what = ["ate", "ripped", "farted on", "spit on", "stole"];
+  let subject = ["my homework", "my car", "your gift"];
+  let when = [
+    "before the class",
+    "after work",
+    "during lunch",
+    "while I was studying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whaIndex = Math.floor(Math.random() * what.length);
+  let subIndex = Math.floor(Math.random() * subject.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    what[whaIndex] +
+    " " +
+    subject[subIndex] +
+    "" +
+    when[whenIndex]
+  );
+}
